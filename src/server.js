f08647d61c2c14b02df28629647a152673b2430e
Joel Hoelting <joelhoelting@protonmail.com>
2019-05-27 23:51:45 -0400

    Remove eb config files

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const emailRouter = require('./routers/email');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
