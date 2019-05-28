a34a796de25e607c2cafac87e68ad38c369cae3e
Joel Hoelting <joelhoelting@protonmail.com>
2019-05-27 21:21:01 -0400

    Update node application to render html on index file"

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const emailRouter = require('./routers/email');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(emailRouter);
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../views');
+
+// Set up handlebars engine and views location
+app.set('view engine', 'pug');
+app.set('views', viewsPath);
+
+// Setup directory to serve static assets
+app.use(express.static(publicDirectoryPath));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Joel Hoelting API' });
+});
+
+app.listen(port, () => console.log(`Application is listening on port ${port}!`));
